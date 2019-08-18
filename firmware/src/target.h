5356a88172dcbc5d4cf62197eaec02216922e6c4
Michael Cullen <michael@cullen-online.com>
2019-08-18 02:09:15 +0100

    Firmware: Added missing header file

---
+++ firmware/src/target.h
@@ -0,0 +1,15 @@
+/******************************************************************************
+* ACNodeReader Firmware - Target-specific stuff                               *
+*                                                                             *
+* (c) 2019 London Hackspace                                                   *
+* Released under a 3-clause BSD license. See included LICENSE file.           *
+*                                                                             *
+* (Yes, this one was repurposed from another of my own projects)              *
+******************************************************************************/
+
+#ifndef TARGET_SPECIFIC_H
+#define TARGET_SPECIFIC_H
+
+#include <board_pins.h>
+
+#endif
