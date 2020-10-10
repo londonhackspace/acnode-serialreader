a39db0a2e01edd4ef3d2a8d8781049c888ff294e
Michael Cullen <michael@smokeemittingdiode.co.uk>
2020-10-10 02:03:02 +0100

    Added missing target makefile

---
+++ firmware/targets/Rev1.1.mk
@@ -0,0 +1,41 @@
+###############################################################################
+# Makefile for ACNodeReader Firmware - Rev1.1 Target with atmega88pb          #
+#                                                                             #
+# (c) 2020 London Hackspace                                                   #
+# Released under a 3-clause BSD license. See included LICENSE file.           #
+#                                                                             #
+###############################################################################
+
+# The MCU target as used by GCC
+MCU=atmega88pb
+
+# The programming target - this needs a trunk version of avrdude (or the avrdude.conf from it anyway)
+AVRDUDE_TARGET=m88pb
+#AVRDUDE_PROGRAMMER=atmelice_isp
+AVRDUDE_PROGRAMMER=usbasp
+# slow it down for the first program, at least
+#AVRDUDE_EXTRA_OPTS=-B1000 -U lfuse:w:0xce:m -U hfuse:w:0xd9:m -U efuse:w:0xf3:m -C /data/projects/z80-board/z80-board-tools/avr-toolchain/outdir/etc/avrdude.conf
+AVRDUDE_EXTRA_OPTS=-U lfuse:w:0xce:m -U hfuse:w:0xdf:m -U efuse:w:0xf8:m
+#AVRDUDE_EXTRA_OPTS=-U lfuse:w:0xce:m -U hfuse:w:0xdf:m -U efuse:w:0xf9:m
+
+# The clock frequency, in Hz of the crystal
+FREQUENCY=12000000
+
+# Any extra, target-specific CFLAGS
+CFLAGS_TARGET=
+
+# target-specific sources
+TARGET_SOURCES= src/lm75.c
+
+BOOTSTART=0x1800
+
+fuses:
+	avrdude -p ${AVRDUDE_TARGET} -c $(AVRDUDE_PROGRAMMER) $(AVRDUDE_EXTRA_OPTS) -B1000
+
+# rules to enable and disable debugwire protocol - note that once enabled, you need a debugwire
+# programmer to disable for normal programming!
+debug:
+	avrdude -p ${AVRDUDE_TARGET} -c atmelice_isp -U hfuse:w:0x9f:m
+
+undebug:
+	avrdude -p ${AVRDUDE_TARGET} -c atmelice_dw -U hfuse:w:0xdf:m	
