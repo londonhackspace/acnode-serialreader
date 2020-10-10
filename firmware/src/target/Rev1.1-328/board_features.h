b5c6f22db562e831769ce4638173425cf9f61bb5
Michael Cullen <michael@smokeemittingdiode.co.uk>
2020-10-10 01:13:44 +0100

    Add Rev1.1 board support to firmware
    This board runs at 12MHz to be in spec for 3.3V operation of the
    microcontroller

---
+++ firmware/src/target/Rev1.1-328/board_features.h
@@ -0,0 +1,22 @@
+/******************************************************************************
+* ACNodeReader Firmware - Rev1.1 (w/328pb) Features                             *
+*                                                                             *
+* (c) 2020 London Hackspace                                                   *
+* Released under a 3-clause BSD license. See included LICENSE file.           *
+*                                                                             *
+******************************************************************************/
+
+#ifndef BOARD_FEATURES_H
+#define BOARD_FEATURES_H
+
+#ifndef TARGET_SPECIFIC_H
+#error Please include target.h instead
+#endif
+
+// SI7060 not available
+#define HAS_DISPLAY 1 // may or may not be fitted
+#define HAS_PWM_LEDS 1
+#define LEDS_COMMON_ANODE 1
+#define HAS_LM75 1
+
+#endif
